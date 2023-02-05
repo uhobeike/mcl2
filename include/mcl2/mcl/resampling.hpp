ee93fa305a9a466b59d2fdd02a0a8aca85e4ebd7
uhobeike <beike315@icloud.com>
2023-02-06 00:41:23 +0900

    Update resampling

---
+++ include/mcl2/mcl/resampling.hpp
@@ -0,0 +1,14 @@
+#ifndef MCL2__RESAMPLING_HPP_
+#define MCL2__RESAMPLING_HPP_
+
+namespace mcl
+{
+class Resampling
+{
+public:
+  Resampling();
+  ~Resampling();
+};
+}  // namespace mcl
+
+#endif
---
+++ src/mcl/resampling.cpp
@@ -0,0 +1,7 @@
+#include "mcl2/mcl/resampling.hpp"
+
+namespace mcl
+{
+Resampling::Resampling(){};
+Resampling::~Resampling(){};
+}  // namespace mcl
